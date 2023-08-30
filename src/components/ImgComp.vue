ef65f24b30aded2f7e222269b434328c2d0d6e2c
shellingfordly <shellingfordly@qq.com>
2023-08-31 01:58:54 +0800

    feat: add ImgComp

---
+++ src/components/ImgComp.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineProps<{ src: string }>();
+</script>
+
+<template>
+  <div class="flex-center border-rd-5 bg-[var(--c-container)]">
+    <img class="margin-auto ma sm:max-w-500px max-h-400px" :src="src" />
+  </div>
+</template>
