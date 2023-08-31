c30186527e4dc175d74460efa090b8eef0f00e4a
shellingfordly <shellingfordly@qq.com>
2023-08-31 22:09:38 +0800

    chore: update build way

---
+++ scripts/rss.ts
@@ -0,0 +1,80 @@
+import { dirname } from "path";
+import fg from "fast-glob";
+import fs from "fs-extra";
+import matter from "gray-matter";
+import MarkdownIt from "markdown-it";
+import type { FeedOptions, Item } from "feed";
+import { Feed } from "feed";
+
+const DOMAIN = "https://antfu.me";
+const AUTHOR = {
+  name: "Shellingfordly",
+  email: "Shellingfordly@qq.com",
+  link: DOMAIN,
+};
+const markdown = MarkdownIt({
+  html: true,
+  breaks: true,
+  linkify: true,
+});
+
+async function run() {
+  await buildBlogRSS();
+}
+
+async function buildBlogRSS() {
+  const files = await fg("pages/*.md");
+
+  const options = {
+    title: "Shellingfordly",
+    description: "Shellingfordly' Blog",
+    id: "https://antfu.me/",
+    link: "https://antfu.me/",
+    copyright: "CC BY-NC-SA 4.0 2021 © Shellingfordly",
+  };
+  const posts: any[] = (
+    await Promise.all(
+      files
+        .filter((i) => !i.includes("index"))
+        .map(async (i) => {
+          const raw = await fs.readFile(i, "utf-8");
+          const { data, content } = matter(raw);
+
+          if (data.lang !== "en") return;
+
+          const html = markdown
+            .render(content)
+            .replace('src="/', `src="${DOMAIN}/`);
+
+          if (data.image?.startsWith("/")) data.image = DOMAIN + data.image;
+
+          return {
+            ...data,
+            date: new Date(data.date),
+            content: html,
+            author: [AUTHOR],
+            link: DOMAIN + i.replace(/^pages(.+)\.md$/, "$1"),
+          };
+        })
+    )
+  ).filter(Boolean);
+
+  posts.sort((a, b) => +new Date(b.date) - +new Date(a.date));
+
+  await writeFeed("feed", options, posts);
+}
+
+async function writeFeed(name: string, options: FeedOptions, items: Item[]) {
+  options.author = AUTHOR;
+
+  const feed = new Feed(options);
+
+  items.forEach((item) => feed.addItem(item));
+
+  await fs.ensureDir(dirname(`./dist/${name}`));
+  await fs.writeFile(`./dist/${name}.xml`, feed.rss2(), "utf-8");
+  await fs.writeFile(`./dist/${name}.atom`, feed.atom1(), "utf-8");
+  await fs.writeFile(`./dist/${name}.json`, feed.json1(), "utf-8");
+}
+
+run();
