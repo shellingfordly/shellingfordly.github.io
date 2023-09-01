0abb119a69cedc6c299ab84159e56b67797fd0a2
shellingfordly <shellingfordly@qq.com>
2023-09-01 14:31:46 +0800

    fix: modify less file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
-import "animate.css";
-import "@unocss/reset/tailwind.css";
+
 import "./styles/index.less";
-import "ol/ol.css";
-import "uno.css";
 import { ViteSSG } from "vite-ssg";
 import App from "./App.vue";
 import NProgress from "nprogress";
