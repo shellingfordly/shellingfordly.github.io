889bdb7e0a09497515276a87414c684bcf479590
shellingfordly <shellingfordly@qq.com>
2024-01-26 15:46:39 +0800

    chore: add markdown-it-github-alerts css

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+import "markdown-it-github-alerts/styles/github-colors-light.css";
+import "markdown-it-github-alerts/styles/github-colors-dark-class.css";
+import "markdown-it-github-alerts/styles/github-base.css";
+import 'shikiji-twoslash/style-rich.css'
 import "./styles/index.less";
 import "uno.css";
 
